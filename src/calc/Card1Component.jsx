fbbc1ee52041bd029bd89e2ac89f02cc30fbf0fd
drkadam07 <124194848+drkadam07@users.noreply.github.com>
2025-01-10 17:29:32 +0530

    create card1 and perform mathematical operations

---
+++ src/calc/Card1Component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Card1Component = ({ input1, setInput1, input2, setInput2, handleCalculation }) => (
+    <>
+        <input
+            type="number"
+            value={input1}
+            onChange={(e) => setInput1(e.target.value)}
+            placeholder="Enter number 1"
+            className="w-full p-2 border border-gray-300 rounded-md mb-2"
+        />
+        <input
+            type="number"
+            value={input2}
+            onChange={(e) => setInput2(e.target.value)}
+            placeholder="Enter number 2"
+            className="w-full p-2 border border-gray-300 rounded-md"
+        />
+        <div className="flex justify-center mb-3 mt-4">
+            <button
+                onClick={handleCalculation}
+                className="bg-blue-500 text-white px-6 py-3 rounded-lg hover:bg-blue-600 focus:outline-none"
+            >
+                Calculate
+            </button>
+        </div>
+
+    </>
+);
+
+export default Card1Component;

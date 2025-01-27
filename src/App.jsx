29e1944ca4dfb59ab5efb924882562870806a896
Drkadam07 <drkadam04@gmail.com>
2025-01-27 22:34:11 +0530

    updated path

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import { ToastContainer } from "react-toastify";  // Import ToastContainer
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import { ToastContainer } from 'react-toastify';
 import Layout from './components/Layout/Layout';
-import Cardcover from "./components/Cards/Cardcover";
-import Contact from "./components/Contact";
+import Cardcover from './components/Cards/Cardcover';
+import Contact from './components/Contact';
 
+import { ThemeProvider } from './components/context/ThemeContext'; // Import the ThemeProvider
 import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/contact" element={<Layout><Contact/></Layout>}/>
-        <Route path="/tools" element={<Layout><Cardcover/></Layout>} />
-      </Routes>
-      
-      <ToastContainer />
-    </Router>
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Layout><Contact /></Layout>} />
+          <Route path="/tools" element={<Layout><Cardcover /></Layout>} />
+        </Routes>
+        <ToastContainer />
+      </Router>
+    </ThemeProvider>
   );
 }
+
 export default App;

d374e8b5d15f09d9b58188f5786fa3be65779a9a
Drkadam07 <drkadam04@gmail.com>
2025-02-13 00:09:06 +0530

    Added default 404

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ import Layout from './components/Layout/Layout';
 import Cardcover from './components/Cards/Cardcover';
 import Contact from './components/Contact';
 
-import { ThemeProvider } from './components/context/ThemeContext'; // Import the ThemeProvider
+import { ThemeProvider } from './components/context/ThemeContext'; 
 import 'react-toastify/dist/ReactToastify.css';
+import Page_FourZeroFour from './components/Page_FourZeroFour';
 
 function App() {
   return (
@@ -17,7 +18,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="contact" element={<Layout><Contact /></Layout>} />
           <Route path="tools" element={<Layout><Cardcover /></Layout>} />
-          <Route path="*" element={<Layout></Layout>} />
+          <Route path="*" element={<Layout><Page_FourZeroFour/></Layout>} />
         </Routes>
         <ToastContainer />
       </Router>

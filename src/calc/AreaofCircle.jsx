def9703523146628e63ac96d21f2ec44792b401e
Drkadam07 <drkadam04@gmail.com>
2025-02-15 23:51:14 +0530

    to claculate the area of circle

---
+++ src/calc/AreaofCircle.jsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import { useTheme } from '../components/context/ThemeContext';
+import { toast } from 'react-toastify';
+
+const AreaofCircle = () => {
+  const { theme } = useTheme();
+  const [input1, setInput1] = useState(null);
+  const [result, setResult] = useState(null);
+
+  const handleCalculation = () => {
+    const p = 3.14;
+    const num1 = parseFloat(input1);
+    if (!isNaN(num1)) {
+      setResult(p * input1 * input1);
+    
+    } else {
+      toast.error('Please enter valid numbers');
+      setResult(null);
+    }
+  }
+
+  return (
+    <>
+      <input
+        type="number"
+        value={input1}
+        onChange={(e) => setInput1(e.target.value)}
+        placeholder="Enter the Radius "
+        className={`w-full p-3 border rounded-md mb-3 focus:outline-none focus:ring-2 focus:ring-blue-500 ${theme === 'light' ? 'bg-white text-black border-gray-300' : 'bg-gray-800 text-white border-gray-600'}`}
+      />
+
+      <div className="flex justify-center mb-3 mt-4">
+        <button
+          onClick={handleCalculation}
+          className="bg-gradient-to-r from-green-400 to-blue-500 text-white px-6 py-3 rounded-lg hover:from-blue-500 hover:to-green-600 focus:outline-none transition duration-300"
+        >
+          Calculate
+        </button>
+      </div>
+      {result && (
+        <div className="text-center mt-4">
+          <p className="font-semibold text-lg">
+            Area of Rectangle: {result}
+          </p>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AreaofCircle;
